c69188c683c4a2250ae30ddfd413e61e1691970b
Diego Inácio <62408076+iamdiegoinacio@users.noreply.github.com>
2021-01-15 13:42:23 -0200

    feat: add div overlay for the serie background in the top

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -4,8 +4,14 @@ import './FeaturedMovie.css';
 
 export default({item}) => {
     return(
-        <section className="featured">
-            <div>{item.original_name}</div>
+        <section className="featured" style={{
+            backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
+        }}>
+            <div className="featured--vertical">
+                <div className="featured--horizontal">
+                    
+                </div>
+            </div>
         </section>
     );
 }
