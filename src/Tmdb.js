bdf6abafe2ef8ed632a62a09db6e3ec124a6c7eb
Diego Inácio <62408076+iamdiegoinacio@users.noreply.github.com>
2021-01-14 23:03:44 -0200

    feat: add Tmdb.js and configure movie API TMDB

---
+++ src/Tmdb.js
@@ -0,0 +1,30 @@
+const API_KEY = '9f3dd533abd910ed3bca22ff26c58d66';
+const API_BASE = 'https://api.themoviedb.org/3';
+
+export default{
+}
+
+
+/*
+PT-BR
+-originais da netflix 
+-recomendados
+-em alta 
+-generos 
+    -ação 
+    -comédia 
+    -terror 
+    -romance 
+    -documentários 
+
+ENGLISH
+-netflix originals
+-treding
+-top rated
+-genres)
+    -action
+    -comedy
+    -horror
+    -romance
+    -documentary
+*/
