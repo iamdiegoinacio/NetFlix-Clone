956ba041092249f3d644773d60331de975f58ddd
Diego Inácio <62408076+iamdiegoinacio@users.noreply.github.com>
2021-01-15 16:06:02 -0200

    feat: add struct header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,17 @@ import './Header.css';
 
 export default () => {
     return (
-        <h1></h1>
+        <header className="header--background">
+            <div className="header--logo">
+                <a href="/">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png" alt="Netflix logo"/>
+                </a>
+            </div>
+            <div className="header--user">
+                <a href="/">
+                    <img src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png" alt="User photo"/>
+                </a>
+            </div>
+        </header>
     )
 }
