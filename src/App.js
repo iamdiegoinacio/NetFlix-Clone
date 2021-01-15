1c2fc0422ee02c33536fa74114581a4d8982b325
Diego Inácio <62408076+iamdiegoinacio@users.noreply.github.com>
2021-01-15 10:54:16 -0200

    feat: create components paste and files MovieRow.js, MovieRow.css, and import the MovieRow in the App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import React, { useEffect }from 'react';
+import React, { useEffect, useState }from 'react';
 import Tmdb from './Tmdb'
+import MovieRow from './components/MovieRow';
 
 
 export default() => {
 
+  const [movieList, setMovieList] = useState([]);
+
   useEffect(() => {
-    const loadAll = async () => {
+    const loadAll = async() => {
       let list = await Tmdb.getHomeList();
-      console.log(list);
+      setMovieList(list);
     }
 
     loadAll();
   }, []);
 
   return (
-    <div>
-      Hello World!
+    <div className="page">
+      <section className="lists">
+        {movieList.map ((item, key) => (
+          <MovieRow  key={key} />
+        ))}
+      </section>
     </div>
   );
 }
