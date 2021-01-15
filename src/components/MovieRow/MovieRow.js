5dd89a1e7fa9db50c18b8cefd8fa6d6f0f5d2825
Diego Inácio <62408076+iamdiegoinacio@users.noreply.github.com>
2021-01-15 19:49:50 -0200

    feat: import NavigateBeforeIcon and NavigateNextIcon, add icons in the file MovieRow.js

--- src/components/MovieRow/MovieRow.js
+++ src/components/MovieRow/MovieRow.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './MovieRow.css';
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
 export default({title, items}) => {
     return (
         <div className="movieRow">
             <h2>{title}</h2>
+            <div className="movieRow--left">
+              <NavigateBeforeIcon style={{fontSize: 50}}/>
+            </div>
+
+            <div className="movieRow--right">
+              <NavigateNextIcon style={{fontSize: 50}}/>
+            </div>
             <div className="movieRow--listarea">
               <div className="movieRow--list">
                 {items.results.length > 0 && items.results.map((item, key) => (
