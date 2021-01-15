cc7db3b870463b7c9faf1904993b116bbba10651
Diego Inácio <62408076+iamdiegoinacio@users.noreply.github.com>
2021-01-15 10:08:39 -0200

    feat: add of links in the items attribute, in the async function getHomeList in the Tmdb.js

--- src/Tmdb.js
+++ src/Tmdb.js
@@ -12,44 +12,44 @@ export default{
         return [
             {
                 slug: 'originals',
-                title: 'Originais do Netflix',
-                items: []
+                title : "Originais do Netflix",
+                items : await basicFecth(`/discover/tv/?with_network=213&language=pt-BR&api_key=${API_KEY}`)
             },
             {
-                slug: 'treding',
-                title: 'Recomendados para você',
-                items: []
+                slug: 'trending',
+                title : "Recomendados para Você",
+                items : await basicFecth(`/trending/all/week?language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'toprated',
-                title: 'Em alta',
-                items: []
+                title : "Em Alta",
+                items : await basicFecth(`/movie/top_rated?&language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'action',
-                title: 'Ação',
-                items: []
+                title : "Ação",
+                items : await basicFecth(`/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'comedy',
-                title: 'Comédia',
-                items: []
+                title : "Comédia",
+                items : await basicFecth(`/discover/movie?with_genres=35&language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'horror',
-                title: 'Terror',
-                items: []
+                title : "Terror",
+                items : await basicFecth(`/discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'romance',
-                title: 'Romance',
-                items: []
-            },
+                title : "Romance",
+                items : await basicFecth(`/discover/movie?with_genres=10749&language=pt-BR&api_key=${API_KEY}`)
+            },            
             {
                 slug: 'documentary',
-                title: 'Documentário',
-                items: []
-            }
+                title : "Documentários",
+                items : await basicFecth(`/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`)
+            },
         ];
     }
 }
