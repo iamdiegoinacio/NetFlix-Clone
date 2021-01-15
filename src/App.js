68a49a6fd0204053c6d8678540d57c0a5b18a72a
Diego Inácio <62408076+iamdiegoinacio@users.noreply.github.com>
2021-01-15 11:56:36 -0200

    feat: add props in the file MovieRow.js and add component MovieRow in the file App.js

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ export default() => {
     <div className="page">
       <section className="lists">
         {movieList.map ((item, key) => (
-          <MovieRow  key={key} />
+          <MovieRow key={key} title={item.title} items={item.items} />
         ))}
       </section>
     </div>
